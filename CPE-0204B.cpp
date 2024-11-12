ff63d83dcef3f7e1520589600e1362ff1d22438b
FUYONG <x123xx777+git@gmail.com>
2024-11-12 21:36:14 +0800

    重新整理縮排

--- CPE-0204B.cpp
+++ CPE-0204B.cpp
@@ -10,72 +10,72 @@ int main(){
     string action = "Nnoe"; // 記錄將要運算的符號ex: plus, minus
     
 
-    while(cin >> s){
-  	    f = ' ' + s + ' ';
-  	    // cout << f << endl;
+while(cin >> s){
+    f = ' ' + s + ' ';
+    // cout << f << endl;
 
-        // 初始化
-  	    num = 0;
-    	sum = 0;
-        
-  	    // analyze
-  	    for(char c: f){
+    // 初始化
+    num = 0;
+    sum = 0;
 
-      		if(isdigit(c)){
-  		    	num = num * 10 + (int)(c - '0');
-  		    }      
-  		    else if(isalpha(c)){ // 遇到英文字母 選擇要進行的運算
-  			    switch(c){
-  				    case 'k':
-  					    num *= 1000;
-  					    break;
-  				    case 'K':
-  					    num *= 1024;
-  					    break;
-  				    case 'm':
-  					    num *= 1000*1000;
-  					    break;
-  				    case 'M':
-  					    num *= 1024*1024;
-  					    break;
-  				    case 'g':
-  					    num *= 1000*1000*1000;
-  					    break;
-  				    case 'G':
-  					    num *= 1024*1024*1024;
-  					    break;
-  			    }
-  		    }
-  		    else{
-                // 如果不是英文字母, 檢查是否需要運算
-  			    if(action == "plus"){
-  				    sum += num;
-  			        num = 0;
-  			    }
-  			    else if(action == "minus"){
-  				    sum -= num;		
-  				    num = 0;
-  			    }
-  		    }
+    // analyze
+    for(char c: f){
 
-            // 當 c 是以下運算符號，則記錄下一次要做的運算
-  		    switch(c){
-  		    	case '+':
-  		    		action = "plus";
-  		    		break;
-  		    	case '-':
-  			    	action = "minus";
-  			    	break;
-  			    case ' ':
-  				    action = "plus";
-  				    break;
-  		    }
-  	    }
+        if(isdigit(c)){
+            num = num * 10 + (int)(c - '0');
+        }      
+        else if(isalpha(c)){ // 遇到英文字母 選擇要進行的運算
+            switch(c){
+                case 'k':
+                    num *= 1000;
+                    break;
+                case 'K':
+                    num *= 1024;
+                    break;
+                case 'm':
+                    num *= 1000*1000;
+                    break;
+                case 'M':
+                    num *= 1024*1024;
+                    break;
+                case 'g':
+                    num *= 1000*1000*1000;
+                    break;
+                case 'G':
+                    num *= 1024*1024*1024;
+                    break;
+            }
+        }
+        else{
+            // 如果不是英文字母, 檢查是否需要運算
+            if(action == "plus"){
+                sum += num;
+                num = 0;
+            }
+            else if(action == "minus"){
+                sum -= num;		
+                num = 0;
+            }
+        }
 
-  	    cout << sum << endl; // 印出答案
+        // 當 c 是以下運算符號，則記錄下一次要做的運算
+        switch(c){
+            case '+':
+                action = "plus";
+                break;
+            case '-':
+                action = "minus";
+                break;
+            case ' ':
+                action = "plus";
+                break;
+        }
     }
 
-    return 0;
+    cout << sum << endl; // 印出答案
+}
+
+return 0;
 }
 
 
@@ -86,39 +86,39 @@ int main(){
 using namespace std;
 
 int main(){
-    string s, action = "none";
-    int num = 0, ans = 0;
+string s, action = "none";
+int num = 0, ans = 0;
 
-    while(cin >> s){
-        s = ' ' + s + ' ';
+while(cin >> s){
+s = ' ' + s + ' ';
 
-        for(char c : s){
-            if(isdigit(c)){
-                num = num*10 + (int)(c - '0');
-            }
-            else if(isalpha(c)){
-                switch(c){
-                    case 'k':
-                        num *= 1000;
-                        break;
-                    
-                }
+for(char c : s){
+if(isdigit(c)){
+num = num*10 + (int)(c - '0');
+}
+else if(isalpha(c)){
+switch(c){
+case 'k':
+num *= 1000;
+break;
 
-            }
-            else{
-                if(action == "plus"){
-                    ans += num;
-                    num = 0;
-                }
-                else if(action == "minus"){
-                    ans -= num;
-                    num = 0;
-                }
+}
 
-            }
+}
+else{
+if(action == "plus"){
+ans += num;
+num = 0;
+}
+else if(action == "minus"){
+ans -= num;
+num = 0;
+}
 
-        }
+}
 
-    }
+}
+
+}
 }
 */
